6711569804213a81fad5b99e89ab6720e70f1e40
Wonseop Lee <wonseop.s.lee@gmail.com>
2021-01-06 21:42:48 +0000

    commit

--- express-hello-world/index.js
+++ express-hello-world/index.js
@@ -2,10 +2,6 @@ const express = require('express');
 const app = express();
 
 app.use(function (req, res) {
-<<<<<<< HEAD
-=======
-  console.log(req.method); // delete before commit
->>>>>>> 43a723004874d3977e98fc7aefc5a7901a090eaa
   res.send('Hello World!')
 });
 
