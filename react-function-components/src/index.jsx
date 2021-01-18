af2d3c744537e94539f7f51a1a95d151a8354890
Wonseop Lee <wonseop.s.lee@gmail.com>
2021-01-18 23:31:10 +0000

    updated with JSX syntax

--- react-function-components/src/index.jsx
+++ react-function-components/src/index.jsx
@@ -3,6 +3,6 @@ import ReactDOM from 'react-dom';
 
 const CustomButton = () => <button>Click Me!</button>;
 ReactDOM.render(
-  CustomButton(),
+  <CustomButton />,
   document.querySelector('#root')
 );
