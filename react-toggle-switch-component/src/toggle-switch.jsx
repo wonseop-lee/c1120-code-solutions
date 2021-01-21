15164812f77ca264c10e57f2bea3c2a50da4af52
Wonseop Lee <wonseop.s.lee@gmail.com>
2021-01-21 18:40:07 +0000

    moved unnecessary variables from state to class property

--- react-toggle-switch-component/src/toggle-switch.jsx
+++ react-toggle-switch-component/src/toggle-switch.jsx
@@ -4,36 +4,34 @@ class ToggleSwitch extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      isToggled: false,
-      className: 'notToggled',
-      container: 'container notToggled',
-      onOrOff: 'OFF'
+      isToggled: false
     };
     this.handleToggle = this.handleToggle.bind(this);
   }
 
   handleToggle() {
     const isToggled = this.state.isToggled;
+
     if (isToggled) {
       this.setState({ isToggled: false });
-      this.setState({ className: 'notToggled' });
-      this.setState({ container: 'container notToggled' });
-      this.setState({ onOrOff: 'OFF' });
+      this.className = 'notToggled';
+      this.containerName = 'container notToggled';
+      this.onOrOff = 'OFF';
     } else {
       this.setState({ isToggled: true });
-      this.setState({ className: 'toggled' });
-      this.setState({ container: 'container toggled' });
-      this.setState({ onOrOff: 'ON' });
+      this.className = 'toggled';
+      this.containerName = 'container toggled';
+      this.onOrOff = 'ON';
     }
   }
 
   render() {
     return (
       <div>
-        <div className={this.state.container}>
-          <button onClick={this.handleToggle} className={this.state.className}></button>
+        <div className={this.containerName}>
+          <button onClick={this.handleToggle} className={this.className}></button>
         </div>
-        <p>{this.state.onOrOff}</p>
+        <p>{this.onOrOff}</p>
       </div>
     );
   }
