1e1576207709e703c471f4b05e3f72ef1c0beb21
Wonseop Lee <wonseop.s.lee@gmail.com>
2021-01-21 00:00:12 +0000

    completed react-form-controls

---
+++ react-form-controls/src/index.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class NewsletterForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { email: '' };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({ email: event.target.value });
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+    console.log('state: ', this.state);
+  }
+
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <label>Email</label>
+        <input type="email" state={this.state.email} onChange={this.handleChange}></input>
+        <button>Sign Up</button>
+      </form>
+    );
+  }
+}
+
+ReactDOM.render(
+  <NewsletterForm />,
+  document.querySelector('#root')
+);
